23fdeded6e73e38d911442272ae0874bb03a8df0
trungnguyen30 <trungbgg05@gmail.com>
2023-09-15 10:48:19 +0700

    update 15/9

--- src/components/Layout/Checkout/index.js
+++ src/components/Layout/Checkout/index.js
@@ -1,12 +1,22 @@
 import './Checkout.css';
 import ButtonComponent from '../Components/Button';
+import { CountContext } from '../Components/CountContext/CountContext';
 
 import { MoneyCollectOutlined } from '@ant-design/icons';
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useContext } from 'react';
 import { Input, Row, Col, Form, message } from 'antd';
 const { TextArea } = Input;
 
 function CheckOutArea() {
+    const value = useContext(CountContext);
+    const iniData = Object.freeze({
+        name: '',
+        address: '',
+        phone: '',
+        email: '',
+        note: '',
+    });
+    const [formData, setFormData] = useState(iniData);
     const handleSubmit = (e) => {
         e.preventDefault();
 
@@ -17,6 +27,7 @@ function CheckOutArea() {
             OrderDescription: '2',
             Name: 'Do vat',
         };
+
         const requestOptions = {
             method: 'POST',
             redirect: 'follow',
@@ -27,14 +38,33 @@ function CheckOutArea() {
             },
             body: JSON.stringify(data),
         };
-        fetch('https://localhost:7121/api/VnPay', requestOptions)
-            .then((res) => {
-                console.log(res.redirected);
-            })
-            .then((data) => console.log(data));
+
+        const response = async () => {
+            await fetch('https://localhost:7121/api/VnPay', requestOptions)
+                .then((res) => {
+                    console.log(res);
+                })
+                .then((data) => console.log(data))
+                .catch((error) => {
+                    console.log(error);
+                });
+        };
+        response();
+
+        if (
+            formData.name === '' ||
+            formData.address === '' ||
+            formData.email === '' ||
+            formData.phone === ''
+        ) {
+            message.warning('Vui lòng nhập thông tin!!');
+        }
     };
     const cartItems = JSON.parse(localStorage.getItem('carts'));
+    const addedCart = JSON.parse(localStorage.getItem('addedCart'));
     const [carts, setCarts] = useState(cartItems || []);
+    const [added, setAdded] = useState(addedCart || []);
+    const [completed, setCompleted] = useState(false);
     const total = useMemo(() => {
         if (carts) {
             return carts.reduce((total, element) => {
@@ -42,10 +72,35 @@ function CheckOutArea() {
             }, 0);
         }
     }, [carts]);
+    const handleAddChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+    const handleShipCod = () => {
+        if (
+            formData.name === '' ||
+            formData.address === '' ||
+            formData.email === '' ||
+            formData.phone === ''
+        ) {
+            message.warning('Vui lòng nhập thông tin cần thiết!!');
+        } else {
+            message.success('Thanh toán thành công!!');
+            setFormData(iniData);
+            setCompleted(true);
+            setCarts([]);
+            setAdded([]);
+            value.setCountCart(0);
+        }
+    };
+    localStorage.setItem('carts', JSON.stringify(carts));
+    localStorage.setItem('addedCart', JSON.stringify(added));
     return (
         <div className="checkout-area py-100">
             <div className="container">
-                {carts.length > 0 && (
+                {!completed && carts.length > 0 && (
                     <div>
                         <Row gutter={32}>
                             <Col xs={14} lg={14} md={24} sm={24}>
@@ -54,71 +109,54 @@ function CheckOutArea() {
                                     <Form layout="vertical">
                                         <Row gutter={[32, 16]}>
                                             <Col xs={24} lg={24} md={24} sm={24}>
-                                                <Form.Item
-                                                    label="Họ và tên"
-                                                    name="name"
-                                                    rules={[
-                                                        {
-                                                            required: true,
-                                                            message: 'Hãy nhập họ tên',
-                                                        },
-                                                    ]}
-                                                >
-                                                    <Input size="large" />
+                                                <Form.Item label="Họ và tên" required>
+                                                    <Input
+                                                        name="name"
+                                                        value={formData.name}
+                                                        onChange={handleAddChange}
+                                                        size="large"
+                                                    />
                                                 </Form.Item>
                                             </Col>
                                             <Col xs={24} lg={24} md={24} sm={24}>
-                                                <Form.Item
-                                                    label="Địa chỉ"
-                                                    name="address"
-                                                    rules={[
-                                                        {
-                                                            required: true,
-                                                            message: 'Hãy nhập địa chỉ',
-                                                        },
-                                                    ]}
-                                                >
-                                                    <Input size="large" />
+                                                <Form.Item label="Địa chỉ" required>
+                                                    <Input
+                                                        name="address"
+                                                        value={formData.address}
+                                                        onChange={handleAddChange}
+                                                        size="large"
+                                                    />
                                                 </Form.Item>
                                             </Col>
                                             <Col xs={12} lg={12} md={24} sm={24}>
-                                                <Form.Item
-                                                    label="Điện thoại"
-                                                    name="phone"
-                                                    rules={[
-                                                        {
-                                                            required: true,
-                                                            message: 'Hãy nhập số điện thoại',
-                                                        },
-                                                    ]}
-                                                >
-                                                    <Input size="large" />
+                                                <Form.Item label="Điện thoại" required>
+                                                    <Input
+                                                        name="phone"
+                                                        value={formData.phone}
+                                                        onChange={handleAddChange}
+                                                        size="large"
+                                                    />
                                                 </Form.Item>
                                             </Col>
                                             <Col xs={12} lg={12} md={24} sm={24}>
-                                                <Form.Item
-                                                    label="Email"
-                                                    name="email"
-                                                    rules={[
-                                                        {
-                                                            required: false,
-                                                        },
-                                                    ]}
-                                                >
-                                                    <Input size="large" />
+                                                <Form.Item label="Email" required>
+                                                    <Input
+                                                        name="email"
+                                                        value={formData.email}
+                                                        onChange={handleAddChange}
+                                                        size="large"
+                                                    />
                                                 </Form.Item>
                                             </Col>
                                             <Col xs={24} lg={24} md={24} sm={24}>
-                                                <Form.Item
-                                                    label="Ghi chú"
-                                                    name="note"
-                                                    rules={[
-                                                        {
-                                                            required: false,
-                                                        },
-                                                    ]}
-                                                >
-                                                    <TextArea rows={6} />
+                                                <Form.Item label="Ghi chú">
+                                                    <TextArea
+                                                        name="note"
+                                                        value={formData.note}
+                                                        onChange={handleAddChange}
+                                                        size="large"
+                                                        rows={6}
+                                                    />
                                                 </Form.Item>
                                             </Col>
                                         </Row>
@@ -164,7 +202,11 @@ function CheckOutArea() {
                                         </div>
                                     </div>
                                     <div>
-                                        <ButtonComponent primary2 className="mt-25">
+                                        <ButtonComponent
+                                            onClick={handleShipCod}
+                                            primary2
+                                            className="mt-25"
+                                        >
                                             THANH TOÁN KHI NHẬN HÀNG
                                         </ButtonComponent>
                                     </div>
@@ -203,13 +245,33 @@ function CheckOutArea() {
                         </Row>
                     </div>
                 )}
-                {carts.length === 0 && (
+                {!completed && carts.length === 0 && (
                     <div className="empty text-align-center">
                         <MoneyCollectOutlined className="empty-icon" />
                         <p>Không có sản phẩm trong giỏ hàng</p>
                         <ButtonComponent to={'/shop'} className="btn-add-empty mt-40">
                             Mua ngay
                         </ButtonComponent>
+                    </div>
+                )}
+                {completed && (
+                    <div className="empty text-align-center">
+                        <p
+                            style={{
+                                fontSize: '40px',
+                            }}
+                            className="mb-20"
+                        >
+                            THANH TOÁN THÀNH CÔNG!!
+                        </p>
+                        <div>
+                            <MoneyCollectOutlined className="empty-icon" />
+                        </div>
+                        <div>
+                            <ButtonComponent to={'/shop'} className="btn-add-empty mt-40">
+                                Tiếp tục mua sắm
+                            </ButtonComponent>
+                        </div>
                     </div>
                 )}
             </div>
